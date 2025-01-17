e6b72364f0031bad0e22548a364a5e084c2d1bc5
rofenac <118231149+rofenac@users.noreply.github.com>
2025-01-16 19:29:41 -0800

    Props Setup
    Set up and made templates for props on the various pages of the website. Standardized style with props populating the various components as needed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-import './index.css'
+import PageTemplate from './pagetemplate'
 
-function App() {
+export function App() {
   return (
     <>
-      <h1>Life is Pain</h1>
+      <PageTemplate
+        firstLink="Home"
+        secondLink="About"
+        thirdLink="Services"
+        fourthLink="Contact"
+        pageTitle="Title"
+        pageSubTitle="SubTitle"
+        titleBoxOne="Box One"
+        titleBoxTwo="Box Two"
+        titleBoxThree="Box Three"
+        titleBoxFour="Box Four"
+        titleBoxFive="Box Five"
+        titleBoxSix="Box Six"
+        descriptionBoxOne="Description One"
+        descriptionBoxTwo="Description Two"
+        descriptionBoxThree="Description Three"
+        descriptionBoxFour="Description Four"
+        descriptionBoxFive="Description Five"
+        descriptionBoxSix="Description Six" />
     </>
   )
 }
